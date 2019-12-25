1980bf4af24b4d9d732908d49e682b8e9e3f510b
himanshu272 <himanshu.shekharjha.272@gmail.com>
2019-12-25 16:37:25 +0530

    [add] python decouple

--- festcms/settings.py.example.py
+++ festcms/settings.py.example.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from decouple import config
 
 # import for openshift
 # from urllib.parse import urlparse
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fillitup'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
